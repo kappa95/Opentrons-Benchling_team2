ed8b338af4c8f8f6df40dc85ee6e3c0e031d9f18
kappa95 <42738531+kappa95@users.noreply.github.com>
2020-06-12 14:04:46 +0200

    Add files via upload

--- v1_station_C.py
+++ v1_station_C.py
@@ -15,7 +15,7 @@ metadata = {
 NUM_SAMPLES = 96  # start with 8 samples, slowly increase to 48, then 94 (max is 94)
 PREPARE_MASTERMIX = True
 TIP_TRACK = True  # i want to keep track of tips
-CHECK_TEMP = False
+CHECK_TEMP = True
 
 
 def run(ctx: protocol_api.ProtocolContext):
@@ -38,7 +38,7 @@ def run(ctx: protocol_api.ProtocolContext):
         'mastermix strips')
     tempdeck.set_temperature(4)  # it sets the temp to 4°C
     # my modification
-    #tempdeck.module.wait_for_temp()  Not sure if needed since physical system is waiting for temperature, but I dont know why 
+    # tempdeck.module.wait_for_temp()  Not sure if needed since physical system is waiting for temperature, but I dont know why
     tube_block = ctx.load_labware(
         'opentrons_24_aluminumblock_nest_1.5ml_snapcap', '5',
         '2ml screw tube aluminum block for mastermix + controls')
@@ -107,15 +107,16 @@ resuming.')
     }
     if CHECK_TEMP:
         temp_file_path = folder_path + '/temp_log.json'
-        TempUB = 5 # Upper bound on allowable temperature
-        Tempflag = 0 # Generates log file in case temperature exceeds bound
-        TempLog = {} # For log file data
-        if tempdeck.temperature > 5:
-            ctx.pause('The temperature is not 5°C')
+        TempUB = 5  # Upper bound on allowable temperature
+        Tempflag = 0  # Generates log file in case temperature exceeds bound
+        TempLog = {"time": [], "value": []}  # For log file data
+        if tempdeck.temperature >= TempUB:
+            ctx.pause('The temperature is above 5°C')
             tempdeck.await_temperature(4)  # not sure if needed or we break the protocol
             ctx.resume()
             Tempflag = 1
-            TempLog{datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S:%f") = tempdeck.temperature # Generates Log file data
+            TempLog["time"].append(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S:%f"))
+            TempLog["value"].append(tempdeck.temperature)  # Generates Log file data
 
     if PREPARE_MASTERMIX:
         vol_overage = 1.2  # decrease overage for small sample number
@@ -147,12 +148,14 @@ resuming.')
         p300.blow_out(mm_tube.top())
         p300.touch_tip()
     
-    if tempdeck.temperature > 5 and CHECK_TEMP:
+    if tempdeck.temperature >= TempUB and CHECK_TEMP:
         ctx.pause('The temperature is not 5°C')
         tempdeck.await_temperature(4)  # not sure if needed or we break the protocol
         ctx.resume()
         Tempflag = 1
-        TempLog{datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S:%f")} = tempdeck.temperature # Generates Log file Data
+        TempLog["time"].append(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S:%f"))
+        TempLog["value"].append(tempdeck.temperature)  # Generates Log file data
+        # TempLog{datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S:%f") = tempdeck.temperature # Generates Log file Data
         
     # transfer mastermix to strips
     vol_per_strip_well = num_cols*mm_dict['volume']*1.1
@@ -162,12 +165,13 @@ resuming.')
     for well in mm_strip:
         p300.transfer(vol_per_strip_well, mm_tube, well, new_tip='never')
 # my modification
-    if tempdeck.temperature > 5 and CHECK_TEMP:
+    if tempdeck.temperature >= TempUB and CHECK_TEMP:
         ctx.pause('The temperature is not 5°C')
         tempdeck.await_temperature(4)  # not sure if needed or we break the protocol
         ctx.resume()
         Tempflag = 1
-        TempLog{datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S:%f") }= tempdeck.temperature 
+        TempLog["time"].append(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S:%f"))
+        TempLog["value"].append(tempdeck.temperature)  # Generates Log file data
     # transfer mastermix to plate
     mm_vol = mm_dict['volume']
     pick_up(m20)
@@ -185,12 +189,14 @@ resuming.')
         m20.aspirate(5, d.top(2))  # suck in any remaining droplets on way to trash
         m20.drop_tip()
         # Check temperature at the end of each iteration
-        if tempdeck.temperature > 5:
-        ctx.pause('The temperature is not 5°C')
-        tempdeck.await_temperature(4)  # not sure if needed or we break the protocol
-        ctx.resume()
-        Tempflag = 1
-        TempLog{datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S:%f")}= tempdeck.temperature # Generates Log file Data
+        if tempdeck.temperature >= 5:
+        # if CHECK_TEMP:  # It is needed to simulate on the computer otherwise is always empty
+            ctx.pause('The temperature is above the limits')
+            tempdeck.await_temperature(4)  # not sure if needed or we break the protocol
+            ctx.resume()
+            Tempflag = 1
+            TempLog["time"].append(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+            TempLog["value"].append(tempdeck.temperature)  # Generates Log file data
                 
     # track final used tip
     if TIP_TRACK and ctx.is_simulating():
@@ -202,12 +208,11 @@ resuming.')
         }
         with open(tip_file_path, 'w') as outfile:
             json.dump(data, outfile)
-                
-    #Track Temperatures 
+
+    # Track Temperatures
     if CHECK_TEMP and ctx.is_simulating():
         if not os.path.isdir(folder_path):
             os.mkdir(folder_path)
-        TEMPdata =  TempLog
+        TEMPdata = TempLog
         with open(temp_file_path, 'w') as outfiletemp:
             json.dump(TEMPdata, outfiletemp)
-                
