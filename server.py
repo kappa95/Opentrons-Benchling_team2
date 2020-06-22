2a56b683a8a2eccb42e252a594f47d3acdfff03a
kappa95 <42738531+kappa95@users.noreply.github.com>
2020-06-22 22:13:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import requests
-import json
+import urllib3
+"""For disabling the insecure warnings"""
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def api_get():
@@ -7,15 +9,8 @@ def api_get():
     headers = {
         'Content-Type': 'application/json'
     }
-    file_name = "2a290c8f-933f-4ac0-bebf-ef3e9c65b4c5.json"
-    response = requests.request("GET", url + '?file=' + file_name, headers=headers, verify=False)
-    if response.status_code >= 400:
-        raise Exception(
-            "Server returned status {}. Response:\n{}".format(
-                response.status_code, json.dumps(response.json())
-            )
-        )
-    return response.json()
+    response = requests.get(url, headers=headers, verify=False)
+    return response
 
 
 def api_post(payload={}):
@@ -23,11 +18,5 @@ def api_post(payload={}):
     headers = {
         'Content-Type': 'application/json'
     }
-    response = requests.request("POST", url, headers=headers, data=payload, verify=False)
-    if response.status_code >= 400:
-        raise Exception(
-            "Server returned status {}. Response:\n{}".format(
-                response.status_code, json.dumps(response.json())
-            )
-        )
-    return response.json()
+    response = requests.post(url, headers=headers, data=payload, verify=False)
+    return response
