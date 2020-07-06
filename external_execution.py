aa53abd2048f4a5da09f80bea2c14b2873ebea7b
Zack Bright <zack@multiplylabs.com>
2020-07-06 14:04:41 -0700

    add in file copy

--- external_execution.py
+++ external_execution.py
@@ -1,4 +1,5 @@
 import paramiko as pk
+import datetime
 
 key_name = 'ot2_ssh_key'  # Key name
 # direct = 'C:/Users/inse9/'  # Folder of the key
@@ -7,6 +8,8 @@ key = direct + key_name
 protocol_folder = '/var/lib/jupyter/notebooks/'  # Folder in which is contained the protocol on the machine
 protocol_file = 'v1_station_C.py'
 
+remote_log_filepath = '/var/lib/jupyter/outputs/temp_log.json'
+local_filepath = "./logs/"
 
 def main(w_ip='192.168.1.14'):  # IP used for ssh-ing the robot
     client = pk.SSHClient()  # Create an object SSH client
@@ -14,7 +17,13 @@ def main(w_ip='192.168.1.14'):  # IP used for ssh-ing the robot
     client.connect(w_ip, username='root', key_filename=key, password='opentrons')  # Connection
     chann = client.invoke_shell()
     chann.send('opentrons_execute {}/{} -n \n'.format(protocol_folder, protocol_file))
-    client.close()
+    chann.send('exit')
+    chann.recv_exit_status()
+    ftp_client = client.open_sftp()
+    localfilepath = "./log_{}.json".format(datetime.datetime.now().strftime("%m-%d-%Y_%H_%M_%S"))
+    ftp_client.get(remotefileth, localfilepath)
+    ftp_client.close()
+
 
 if __name__ == "__main__":
     main()
