ec987a58f3d2ae4cb7b596a5a0d70c6c36782cd5
Zack Bright <zack@multiplylabs.com>
2020-07-06 14:08:30 -0700

    test v ersinos

--- external_execution.py
+++ external_execution.py
@@ -15,7 +15,8 @@ def main(w_ip='192.168.1.14'):  # IP used for ssh-ing the robot
     client.set_missing_host_key_policy(pk.AutoAddPolicy())  # It is needed to add the device policy
     client.connect(w_ip, username='root', key_filename=key, password='opentrons')  # Connection
     chann = client.invoke_shell()
-    chann.send('opentrons_execute {}/{} -n \n'.format(protocol_folder, protocol_file))
+    chann.send('ls -a')
+    # chann.send('opentrons_execute {}/{} -n \n'.format(protocol_folder, protocol_file))
     chann.send('exit \n')
     chann.recv_exit_status()
     ftp_client = client.open_sftp()
